6640de0ce3fca933bf9d63350f71c3c30951e848
A Wegner <mosquito@team-vipers.com>
2014-05-29 21:42:35 -0500

    Updates template, adds comments to DB

--- FlaskPanel/app.py
+++ FlaskPanel/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
 from models.secomments import Comment, CommentType
 from SEAPI.SEAPI import SEAPI, SEAPIError
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 app = Flask(__name__)
 app.config.from_pyfile('app.cfg')
@@ -16,37 +18,41 @@ post_type_dict = {
 @app.route('/')
 def index():
     return render_template('index.html',
-                            #comments=db.session.query(Comment).order_by(Comment.creation_date.desc()).all())
-                            comments=db.session.query(Comment).filter_by(id=0).order_by(Comment.creation_date.desc()).all())
+                            comments=db.session.query(Comment).filter_by(id=0).order_by(Comment.creation_date.desc()).all(),
+                            pagetitle="Main")
 
 
 @app.route('/training_data')
 def training_data():
     return render_template('index.html',
-                            comments=db.session.query(Comment).filter_by(is_training=True).order_by(Comment.creation_date.desc()).all())
-                            
-                            
+        comments=db.session.query(Comment).filter_by(is_training=True).order_by(Comment.creation_date.desc()).all(),
+        pagetitle="Training Data")
+
+
 @app.route('/disputed_comments')
 def disputed_data():
     return render_template('index.html',
-                            comments=db.session.query(Comment).filter_by(disputed=True).order_by(Comment.creation_date.desc()).all())
+                            comments=db.session.query(Comment).filter_by(disputed=True).order_by(Comment.creation_date.desc()).all(),
+                            pagetitle="Disputed Comments")
 
-                            
+
 @app.route('/automated_data')
 def automated_data():
     return render_template('index.html',
-                            comments=db.session.query(Comment).filter_by(added_manually=False, is_training=False).order_by(Comment.creation_date.desc()).all())
+                            comments=db.session.query(Comment).filter_by(added_manually=False, is_training=False).order_by(Comment.creation_date.desc()).all(),
+                            pagetitle="Automatically Added Comments")
 
 
 @app.route('/manual_data')
 def manual_data():
     return render_template('index.html',
-                            comments=db.session.query(Comment).filter_by(added_manually=True, is_training=False).order_by(Comment.creation_date.desc()).all())
+                            comments=db.session.query(Comment).filter_by(added_manually=True, is_training=False).order_by(Comment.creation_date.desc()).all(),
+                            pagetitle="Manually Added Comments")
 
 
 @app.route('/add_comments')
 def add_comments():
-    return render_template('add_comment.html')
+    return render_template('add_comment.html', pagetitle="Add Comments")
 
     
 @app.route('/add_comment_data', methods=['POST'])
@@ -54,57 +60,67 @@ def add_comment_data():
     comment_list = request.form.getlist('comments[]')
     comment_types_list = request.form.getlist('commenttypes[]')
     comments = []
-    
-    for c in comment_list:
-        comments.append(c.split("#")[1].split("_")[0][7:])
-
-    comment_dict = dict(zip(comments, comment_types_list))
-
-    # THIS IS WHERE WE PULL COMMENTS FROM API
-    site = SEAPI("stackoverflow")
-    comment_data = site.fetch('comments',ids=comments, filter='!1zSsiTKfrlw0eKYQiRXjG')
-    for c in comment_data:
-        link = "http://stackoverflow.com/posts/comments/%s" % (c['comment_id'])
-        text = BeautifulSoup(c['body']).get_text()
-        id = c['comment_id']
-        score = c['score']
-        user_id = c['owner']['user_id']
-        reputation = c['owner']['reputation']
-        post_type = post_type_dict(c['post_type'])
-        creation_date=datetime.strptime(c['creation_date'], "%Y-%m-%d %H:%M:%S")
-        comment_type_id = comment_dict(c['comment_id'])
-        
-        print "-" * 15
-        print """
-        Link => %s
-        Text => %s
-        ID => %s
-        Score => %s
-        User => %s
-        Rep => %s
-        Post Type => %s
-        Creation Date => %s
-        Comment Type => %s
-        """ % (
-        link,
-        text,
-        id,
-        score,
-        user_id,
-        reputation,
-        post_type,
-        creation_date,
-        comment_type_id,
-        )
-        
-        
-#        db.session.add(Comment(link=c.))
-
-    
     response = {
         'success': False,
         'msg': 'Unknown Error'
     }
+
+    if comment_list[0]:
+        for c in comment_list:
+            try:
+                comments.append(c.split("#")[1].split("_")[0][7:])
+            except IndexError:
+                response['msg'] = "Invalid Input. Need full URL to comment."
+                return jsonify(**response)
+
+        comment_dict = dict(zip(comments, comment_types_list))
+
+        # THIS IS WHERE WE PULL COMMENTS FROM API
+        site = SEAPI("stackoverflow")
+        comment_data = site.fetch('comments',ids=comments, filter='!1zSsiTKfrlw0eKYQiRXjG')
+        for c in comment_data['items']:
+            link = "http://stackoverflow.com/posts/comments/%s" % (c['comment_id'])
+            text = BeautifulSoup(c['body']).get_text()
+            id = c['comment_id']
+            score = c['score']
+            user_id = c['owner']['user_id']
+            reputation = c['owner']['reputation']
+            post_type = post_type_dict[c['post_type']]
+            creation_date = datetime.fromtimestamp(c['creation_date'])#.strftime("%Y-%m-%d %H:%M:%S")
+            comment_type_id = comment_dict[unicode(c['comment_id'])]
+
+            # print "-" * 15
+            # print """
+            # Link => %s
+            # Text => %s
+            # ID => %s
+            # Score => %s
+            # User => %s
+            # Rep => %s
+            # Post Type => %s
+            # Creation Date => %s
+            # Comment Type => %s
+            # """ % (
+            # link,
+            # text,
+            # id,
+            # score,
+            # user_id,
+            # reputation,
+            # post_type,
+            # creation_date,
+            # comment_type_id,
+            # )
+
+            db.session.add(Comment(link=link,text=text, id=id, score=score, user_id=user_id, reputation=reputation,
+                          post_type=post_type, creation_date=creation_date, comment_type_id=comment_type_id,added_manually=True))
+
+        try:
+            db.session.commit()
+        except IntegrityError:
+            response['msg'] = "Attempted to enter duplicate comment."
+    else:
+        response['msg'] = "No comments input."
     return jsonify(**response)
     
 #    return render_template('index.html',
