12c371372b99d24e5677405d55a947c93e34e66a
A Wegner <mosquito@team-vipers.com>
2014-06-09 15:06:08 -0500

    Add string output for classes

--- models/secomments.py
+++ models/secomments.py
@@ -34,6 +34,9 @@ class Comment(Base):
     system_add_date = Column(DateTime, nullable=False, default=datetime.datetime.now())
     is_training = Column(Boolean, nullable=False, default=False)
     added_manually = Column(Boolean, nullable=False, default=False)
+	
+	def __repr__(self):
+		return "<Comment(id='%s', text='%s', comment_type='%s', link='%s')>" % (self.id, self.text, self.comment_type, self.link)
 
 
 class CommentType(Base):
@@ -42,6 +45,9 @@ class CommentType(Base):
     id = Column(Integer, primary_key=True, unique=True)
     name = Column(CoerceUTF8(50, convert_unicode=True), nullable=False)
     is_active = Column(Boolean, nullable=False, default=True)
+	
+	def __repr__(self):
+		return "<CommentType(id='%s', name='%s', is_active='%s')>" % (self.id, self.name, self.is_active)
 
 
 class TrainingAlgorithm(Base):
@@ -65,8 +71,13 @@ class TrainingResult(Base):
     classification_date = Column(DateTime, nullable=False, default=datetime.datetime.now())
     is_correct_classification = Column(Boolean)
 
+
 class Setting(Base):
     """This class defines settings that our application uses"""
     __tablename__ = 'settings'
     name = Column(CoerceUTF8(125, convert_unicode=True), primary_key=True, unique=True)
     value = Column(CoerceUTF8(500, convert_unicode=True), nullable=False)
+	
+	def __repr__(self):
+		return "<Setting(name='%s', value='%s')>" % (self.name, self.value)
+
