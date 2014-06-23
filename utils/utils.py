c4be5945eccd6e95e2cf4162564297aebfd61c9a
A Wegner <mosquito@team-vipers.com>
2014-06-23 13:48:19 -0500

    Code clean up

--- utils/utils.py
+++ utils/utils.py
@@ -12,23 +12,7 @@ post_type_dict = {
     "answer": 2
 }
 
-
 def setup_logging(file_name, file_level=logging.INFO, console_level=logging.INFO, requests_level=logging.CRITICAL):
-    logging.basicConfig(filename='logs/%s.log' % (file_name), level=file_level,
-                        format='%(asctime)s - %(levelname)-8s - %(name)-12s - %(message)s',
-                        encoding = "UTF-8")
-    console_log = logging.StreamHandler()
-    console_log.setLevel(console_level)
-    formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
-    console_log.setFormatter(formatter)
-    logging.getLogger('').addHandler(console_log)
-    requests_log = logging.getLogger('requests.packages.urllib3')
-    requests_log.setLevel(requests_level)
-    logging.debug("Logging initialized")
-    return logging
-
-
-def setup_logging_2(file_name, file_level=logging.INFO, console_level=logging.INFO, requests_level=logging.CRITICAL):
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
 
