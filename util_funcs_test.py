e190cde6304b3aa7a7e78e4d631ebd9ce3a8dc66
A Wegner <andy@andrewwegner.com>
2014-07-25 16:56:51 -0500

    Removes unused test file

--- util_funcs_test.py
+++
@@ -1,8 +0,0 @@
-from utils import utils
-from models.secomments import Comment, CommentType, Setting
-
-logging = utils.setup_logging("utils_funcs")
-logging.info("test")
-
-s = utils.connect_to_db("sqlite:///FlaskPanel/se_comments.db")
-print s.query(Setting).filter_by(name='se_api_key').one()
